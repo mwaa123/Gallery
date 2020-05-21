d76873f2047a913a812a2058eb541162f999ed81
ruth <ruthwanjiramugo@gmail.com>
2020-05-22 00:05:32 +0300

    add the added class location and a foreignkey created

--- pic/models.py
+++ pic/models.py
@@ -4,9 +4,13 @@ from django.db import models
 class Image(models.Model):
     name = models.CharField(max_length =30)
     description = models.TextField()
-    category_image = models.ImageField(upload_to = 'image/', blank=True)
+    image = models.ImageField(upload_to = 'image/', blank=True)
 
 class Category(models.Model):
     name = models.CharField(max_length =30)
-    
-    
+    image = models.ForeignKey(Image)
+
+
+
+class Location(models.Model):
+    place = models.CharField(max_length =30)
