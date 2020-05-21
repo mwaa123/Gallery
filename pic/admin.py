f7506f28830907ca2967fece688bc345f3d91e3f
ruth <ruthwanjiramugo@gmail.com>
2020-05-22 02:31:05 +0300

    add the class registration in the admin

--- pic/admin.py
+++ pic/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Image
+from .models import Image,Category,Location
 
-admin.site.register(Image)
+
 
 # Register your models here.
+admin.site.register(Image)
+admin.site.register(Category)
+admin.site.register(Location)
