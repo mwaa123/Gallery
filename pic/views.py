80d3a8b642725a65b0d0dd3a2a3921e45372bbf8
ruth <ruthwanjiramugo@gmail.com>
2020-05-22 11:54:41 +0300

    add a trial on obtaining the data from the database and displaying them

--- pic/views.py
+++ pic/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render,redirect
 from django.http  import HttpResponse
 from django.http  import HttpResponse,Http404
-
+from .models import Image,Category,Location
 # Create your views here.
 
 def welcome(request):
-
-    return render(request,'all-fold/welcome.html')
+    images = Image.objects.all()
+    
+    args={'images':images}
+    return render(request,'all-fold/welcome.html',args)
 
 def people (request):
 
